131cc3652bb26a6c7d5646b82e37352434193ca7
Guilherme. <guilherme.melo4631@gmail.com>
2026-06-15 22:53:00 -0300

    feat: Organizando os assets de Icones

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,23 +1,21 @@
-import AddIcon from "./add.svg?react"
-import CheckIcon from "./check.svg?react"
-import CloudSunIcon from "./cloud-sun.svg?react"
-import DetailIcon from "./details.svg?react"
-import HomeIcon from "./home.svg?react"
-import LoaderIcon from "./loader.svg?react"
-import MoonIcon from "./moon.svg?react"
-import SunIcon from "./sun.svg?react"
-import TasksIcon from "./tasks.svg?react"
-import TrashIcon from "./trash.svg?react"
+import AddIcon from "./add.svg?react";
+import CheckIcon from "./check.svg?react";
+import LoaderIcon from "./loader.svg?react";
+import TasksIcon from "./tasks.svg?react";
+import TrashIcon from "./trash.svg?react";
+import ChevronDownIcon from "./chevron-down.svg?react";
+import LogoutIcon from "./logout.svg?react";
+import DetailsIcon from "./details.svg?react";
+import PencilIcon from "./pencil.svg?react";
 
 export {
   AddIcon,
   CheckIcon,
-  CloudSunIcon,
-  DetailIcon,
-  HomeIcon,
   LoaderIcon,
-  MoonIcon,
-  SunIcon,
   TasksIcon,
+  ChevronDownIcon,
   TrashIcon,
-}
+  DetailsIcon,
+  LogoutIcon,
+  PencilIcon,
+};
