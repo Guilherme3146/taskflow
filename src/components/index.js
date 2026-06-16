fcb12124913329422189bc9cca6eb3395e22d4e1
Guilherme. <guilherme.melo4631@gmail.com>
2026-06-15 22:59:10 -0300

    Refactor: Organizando os Components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import ConfirmDialog from "./ConfirmDialog";
+import Input from "./Input";
+import Sidebar from "./Sidebar";
+import TaskDialog from "./TaskDialog";
+import TaskDetailDialog from "./TaskDetailDialog";
+import TaskItem from "./TaskItem";
+import Tasks from "./Tasks";
+import TaskFilters from "./TasksFilters";
+
+export {
+  Button,
+  ConfirmDialog,
+  Input,
+  Sidebar,
+  TaskDialog,
+  TaskDetailDialog,
+  TaskItem,
+  Tasks,
+  TaskFilters,
+};
